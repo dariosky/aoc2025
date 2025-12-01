8c1031de268d5bf6c8867a1dd53224bc4b9c1f0e
Dario Varotto <dario.varotto@gsr.io>
2025-12-01 11:04:48 +0100

    Day 1: Secret Entrance

---
+++ src/day1/main.py
@@ -0,0 +1,20 @@
+INITIAL_POSITION = 50
+
+
+def compute_code(filename) -> int:
+    zero_positions = 0
+    position = INITIAL_POSITION
+    with open(filename, "r") as file:
+        for line in file:
+            direction, steps = line[0], int(line[1:])
+            steps = steps if direction == "R" else -steps
+            position = (position + steps) % 100
+            if position == 0:
+                zero_positions += 1
+
+    return zero_positions
+
+
+if __name__ == "__main__":
+    assert compute_code("sample.txt") == 3
+    print(compute_code("input.txt"))
